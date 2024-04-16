38c158353142e9d784bfc533bee4656bc906f5b1
amanuela97 <babyzewdie@gmail.com>
2024-04-16 20:58:37 +0300

    lesson 5 completed

--- projects/05-control-flow-for/src/app/app.component.ts
+++ projects/05-control-flow-for/src/app/app.component.ts
@@ -11,24 +11,26 @@ import { Component } from '@angular/core';
           <img class="product-image" src="https://placehold.co/100x100" />
         </div>
         <section class="details">
-          <p class="title"><!-- car make and model--></p>
+          @for(car of carList; track car.make){
+          <p class="title">{{ car.make }}</p>
           <hr />
           <p class="detail">
             <span>Year</span>
-            <span><!-- year --></span>
+            <span>{{ car.year }}</span>
           </p>
           <div class="detail">
             <span>Transmission</span>
-            <span><!-- transmission --></span>
+            <span>{{ car.transmission }}</span>
           </div>
           <p class="detail">
             <span>Mileage</span>
-            <span><!-- miles --></span>
+            <span>{{ car.miles }}</span>
           </p>
           <p class="detail">
             <span>Price</span>
-            <span><!-- price --></span>
+            <span>{{ car.price }}</span>
           </p>
+          }
         </section>
       </article>
     </section>
