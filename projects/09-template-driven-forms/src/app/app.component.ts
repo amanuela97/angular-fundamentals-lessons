99ccd1a7bcadaacad70b06dbcb5e021ae493eb90
amanuela97 <babyzewdie@gmail.com>
2024-04-19 15:02:39 +0300

    lesson 9 completed

--- projects/09-template-driven-forms/src/app/app.component.ts
+++ projects/09-template-driven-forms/src/app/app.component.ts
@@ -1,25 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [FormsModule],
   template: `
     <article>
       <h1>Blog Post</h1>
       <section>
         <label for="title">Post Title</label>
         <!-- add the input -->
+        <input type="text" id="title" [(ngModel)]="title" />
 
         <label for="body">Post Body</label>
         <!-- add the textarea -->
+        <textarea
+          name="post-body"
+          id="body"
+          cols="30"
+          rows="10"
+          [(ngModel)]="body"
+        ></textarea>
       </section>
-      <!-- <section>
-        <p>Display title</p>
-        <p>Display value</p>
-      </section> -->
+      <section>
+        <p>{{ title }}</p>
+        <p>{{ body }}</p>
+      </section>
     </article>
   `,
 })
 export class AppComponent {
-  title = '09-template-driven-forms';
+  title = '';
+  body = '';
 }
